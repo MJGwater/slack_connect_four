d7d82db0947c5929db009884b506ccfe4d328a5d
Matthew Goldwater <mattgwater@gmail.com>
2017-03-20 13:51:06 -0700

    User can play 1 game at a time and Storage can hold multiple games.

--- bot.js
+++ bot.js
@@ -16,7 +16,14 @@ const privateConvoWithPlayer1 = (bot, message) => {
   return (err, convo) => {
     teams.get(message.team, (err2, teamData) => {
       // console.log('teamData is: ', teamData);
-      convo.ask(`${teamData.boardStr} It's your turn. Choose a # between 1-7 to select a column.`);
+      // const theRightGame
+      for (let i = 0; i < teamData.games.length; i++) {
+        if (teamData.games[i].player1 === message.user) {
+          // console.log('i think i\'m never hitting here');
+          const theRightGame = teamData.games[i];
+          convo.ask(`${theRightGame.boardStr} It's your turn. Choose a # between 1-7 to select a column.`);
+        }
+      }
     });
   }
 }
@@ -26,37 +33,55 @@ hears('^play <@([a-z0-9-._]+)>', 'direct_message', (bot, message) => {
   const player1 = message.user;
   const player2 = message.match[1];
   // console.log('player1 is: ', player1, 'player2 is: ', player2);
-  /*
   teams.get(message.team, (err, teamData) => {
     console.log('teamData is: ', teamData);
-    if (teamData.player1 === message.user || teamData.player2 === message.user) {
-      bot.reply('You can\'t start a new game because you\'re already in a game!')
-    }
-  });*/
-  bot.api.users.info({ user: player2 }, (err, player2Info) => {
-    if (player2Info.user.is_bot || player2Info.user.id === 'USLACKBOT') {
-      bot.reply(message, 'Bots can\'t play!');
-    } else {
-      // bot.reply(message, 'Both players are human!');
-      const newBoard = board.newBoardState;
-      console.log('message.team is: ', message.team);
-      const gameData = {
-        id: message.team,
-        player1,
-        player2,
-        boardArr: newBoard,
-        boardStr: board.makeBoard(newBoard),
-        numberInColumn: board.numberInColumn,
-      };
-      // console.log('gameData is: ', gameData);
-      teams.save(gameData, (err1) => {
-        if (err1) throw err1;
-        console.log('teams is: ', teams);
-        // teams.get(message.team, (err2, teamData) => {
-          // console.log('teamData is: ', teamData);
-        bot.startConversation(message, privateConvoWithPlayer1(bot, message));
-      });
-    }
+    const lengthToSearchOver = teamData ? teamData.games.length : 0;
+    let alreadyPlayingGame = '';
+    let opponentPlayingGame = '';
+      for (let i = 0; i < lengthToSearchOver; i++) {
+        if (teamData.games[i].player1 === player1 || teamData.games[i].player2 === player1) {
+          alreadyPlayingGame = true;
+        }
+        if (teamData.games[i].player1 === player2 || teamData.games[i].player2 === player2) {
+          opponentPlayingGame = true
+        }
+      }
+      if (alreadyPlayingGame) {
+        bot.reply(message, 'You can\'t start a new game because you\'re already in a game!');
+      } else if (opponentPlayingGame) {
+        bot.reply(message, `Sorry, <@${player2}> is already playing a game!`);
+      } else {
+          bot.api.users.info({ user: player2 }, (err, player2Info) => {
+            if (player2Info.user.is_bot || player2Info.user.id === 'USLACKBOT') {
+              bot.reply(message, 'Bots can\'t play!');
+            } else {
+              // bot.reply(message, 'Both players are human!');
+              const newBoard = board.newBoardState;
+              console.log('message.team is: ', message.team);
+              teamData = teamData || {
+                id: message.team,
+                games: [],
+              }
+              const gameData = {
+                id: message.team,
+                player1,
+                player2,
+                boardArr: newBoard,
+                boardStr: board.makeBoard(newBoard),
+                numberInColumn: board.numberInColumn,
+              };
+              teamData.games.push(gameData);
+              // console.log('gameData is: ', gameData);
+              teams.save(teamData, (err1) => {
+                if (err1) throw err1;
+                console.log('teams is: ', teams);
+                // teams.get(message.team, (err2, teamData) => {
+                  // console.log('teamData is: ', teamData);
+                bot.startConversation(message, privateConvoWithPlayer1(bot, message));
+              });
+            }
+          });
+      }
   });
 });
 
